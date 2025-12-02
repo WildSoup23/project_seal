f5e1fe66e590bcf6c13f4f443272d90f018048db
JagHatarElgiganten123 <d25antli@student.his.se>
2025-12-02 13:33:58 +0100

    Hållt på med uppgraderings implementation

--- Assets/private folders/Tristan The Goat/Script/Upgrade_Menu_Script.cs
+++ Assets/private folders/Tristan The Goat/Script/Upgrade_Menu_Script.cs
@@ -177,7 +177,7 @@ public class Upgrade_Menu_Script : MonoBehaviour
         {
             if (money >= acceleration_upgrade_cost)
             {
-                acceleration_upgrade++;
+                acceleration_upgrade += 0.1f;
                 money -= acceleration_upgrade_cost;
                 acceleration_upgrade_cost = Mathf.Round(acceleration_upgrade_cost * acceleration_upgrade_cost_increase);
                 acceleration_slider.value = acceleration_upgrade / max_acceleration_upgrade;
