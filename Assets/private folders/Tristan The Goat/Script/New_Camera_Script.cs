7422d2014ae5141c75f3aedd002d00f272e9cf01
WildSoup23 <tristan06blankenfjell@gmail.com>
2025-12-02 10:42:45 +0100

    New camera script + seal trail
    *Added a new camera script that follows the player as long as the player is ahead of the camera.
    
    * The camera now zooms out and in based on the players y pos.
    
    * Added a seal trail prefab where the color and width can be changed in the scripts inspector.
    
    * The trails alpha is based on the players speed, the trail becomes more and more visible when the player goes faster.

---
+++ Assets/private folders/Tristan The Goat/Script/New_Camera_Script.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class New_Camera_Script : MonoBehaviour
+{
+    // Variables
+    [Header("Variables")]
+    [Tooltip("The speed at wich the camera will follow the player and zoom, higher values means faster.")]
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float followSpeed;
+    [Tooltip("Camera y pos offset")]
+    [SerializeField] private float cameraYOffset;
+
+    // Refrences
+    private Camera cm;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Gets main camera in scene
+        cm = Camera.main; 
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        // Moves the camera if the player goes far right enough
+        if (transform.position.x > cm.transform.position.x)
+        {
+            float a = cm.transform.position.x;
+            float b = transform.position.x;
+            cm.transform.position = Vector3.Lerp(
+                new Vector3(a,cm.transform.position.y, cm.transform.position.z), 
+                new Vector3(b, cm.transform.position.y, cm.transform.position.z), 
+                followSpeed);
+        }
+
+        // The cameras zoom and y pos based on player y pos.
+        cm.orthographicSize = Mathf.Clamp(transform.position.y + Mathf.Sqrt(Mathf.Pow(cameraYOffset, 2)), 6, 9999);
+        cm.transform.position = Vector3.Lerp(
+            new Vector3(cm.transform.position.x, cm.transform.position.y, cm.transform.position.z),
+            new Vector3(cm.transform.position.x, Mathf.Clamp(cm.orthographicSize + cameraYOffset, 2,9999), cm.transform.position.z),
+            followSpeed* 2);
+
+    }
+}
