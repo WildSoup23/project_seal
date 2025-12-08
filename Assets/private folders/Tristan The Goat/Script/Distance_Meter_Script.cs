7f455738f2e99613f7b847053eb867d27836e6ca
WildSoup23 <tristan06blankenfjell@gmail.com>
2025-12-08 14:08:16 +0100

    Added isberg to distancce meter
    * Made it so that an icon representing iceberg, start and finish line appear on the distance meter.
    
    * Fixed some volume things
    
    *Maybe updated so that the gui scales correctly

--- Assets/private folders/Tristan The Goat/Script/Distance_Meter_Script.cs
+++ Assets/private folders/Tristan The Goat/Script/Distance_Meter_Script.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Distance_Meter_Script : MonoBehaviour
 {
+    // Varibles
+    [SerializeField] private float iconSize;
+
+
     // Refrences
+    [SerializeField] private GameObject startIcon;
+    [SerializeField] private GameObject isbergIcon;
+    [SerializeField] private GameObject endIcon;
+    private List<GameObject> icons = new List<GameObject>();
+    private float width;
     private RectTransform handle_rect;
     private Slider slider;
     private Transform start;
@@ -18,6 +28,38 @@ public class Distance_Meter_Script : MonoBehaviour
         start = GameObject.FindGameObjectWithTag("Start").GetComponent<Transform>();
         end = GameObject.FindGameObjectWithTag("End").GetComponent<Transform>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        
+        if(isbergIcon != null && startIcon != null && endIcon != null)
+        {
+            width = slider.gameObject.GetComponent<RectTransform>().rect.width;
+            GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("Slope");
+            
+            foreach (GameObject obj in objectsWithTag)
+            {
+                GameObject newItem = Instantiate(isbergIcon, transform);
+                Vector3 newpos = new Vector3(0, 0, 0);
+                newpos.x = (((obj.transform.position.x /
+                    (end.position.x - start.position.x)) * width - (width / 2)) + width/10);
+                newItem.GetComponent<RectTransform>().localPosition = newpos;
+                newItem.transform.SetSiblingIndex(handle_rect.transform.GetSiblingIndex());
+                icons.Add(newItem);
+            }
+            // Start icon
+            GameObject startPoint = Instantiate(startIcon, transform);
+            Vector3 newpos2 = new Vector3(0-(width/2), 0, 0);
+            startPoint.GetComponent<RectTransform>().localPosition = newpos2;
+            startPoint.transform.SetSiblingIndex(handle_rect.transform.GetSiblingIndex());
+            icons.Add(startPoint);
+
+            // End icon
+            GameObject endPoint = Instantiate(endIcon, transform);
+            Vector3 newpos3 = new Vector3(width/2, 0, 0);
+            endPoint.GetComponent<RectTransform>().localPosition = newpos3;
+            endPoint.transform.SetSiblingIndex(handle_rect.transform.GetSiblingIndex());
+            icons.Add(endPoint);
+
+        }
     }
 
     // Update is called once per frame
