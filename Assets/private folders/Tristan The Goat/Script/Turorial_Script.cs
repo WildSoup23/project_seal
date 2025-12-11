18e1f52be7064ab478dfb988d630a5d10aa899fe
WildSoup23 <tristan06blankenfjell@gmail.com>
2025-12-11 10:50:25 +0100

    Tutorial + Menu fixes + scaling

---
+++ Assets/private folders/Tristan The Goat/Script/Turorial_Script.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Turorial_Script : MonoBehaviour
+{
+    private bool hasPlayed = false;
+    private GameObject panel;
+    private const string path = @"c:\temp\tutorial_test.txt";
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        if (File.Exists(path))
+        {
+            hasPlayed = bool.Parse(File.ReadLines(path).First());
+        }
+        
+        panel = transform.Find("the panel").gameObject;
+        if (hasPlayed)
+        {
+            panel.SetActive(false);
+        }
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!hasPlayed)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                hasPlayed = true;
+                panel.SetActive(false);
+                SaveData();
+                Time.timeScale = 1f;
+                rb.simulated = true;
+            }
+        }
+    }
+
+    private void SaveData()
+    {
+        const string path = @"c:\temp\tutorial_test.txt";
+
+        File.Delete(path); // Ensures that we write to a blank file
+
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine(hasPlayed);
+        }
+    }
+}
