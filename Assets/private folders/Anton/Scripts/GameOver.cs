45ca269672d16e5dbedbb6c5ab115457e817dbdb
WildSoup23 <tristan06blankenfjell@gmail.com>
2025-12-17 09:23:27 +0100

    Added sfx + fixed win lose
    * Added space for splash sfx for when player hits water
    
    * Fixed the win lose so that a player cant win then lose.
    
    * Made it so that a player is frozen when they hit either win or lose.

--- Assets/private folders/Anton/Scripts/GameOver.cs
+++ Assets/private folders/Anton/Scripts/GameOver.cs
@@ -1,16 +1,24 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameOver : MonoBehaviour
 {
     public UnityEvent SaveCoins;
-    
+    private AudioSource splash;
+
+    private void Start()
+    {
+        splash = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            if(splash != null)
+            {
+                splash.Play();
+            }
+
             SaveCoins?.Invoke();
             Win_Lose_Script.instance.OnTriggerLose();
         }
--- Assets/private folders/Tristan The Goat/Script/Win_Lose_Script.cs
+++ Assets/private folders/Tristan The Goat/Script/Win_Lose_Script.cs
@@ -10,7 +10,6 @@ public class Win_Lose_Script : MonoBehaviour
     [SerializeField] public GameObject win_lose_panel;
     [SerializeField] private List<Sprite> images = new List<Sprite>();
     [SerializeField] private List<Sprite> images2 = new List<Sprite>();
-    [SerializeField] private List<string> texts = new List<string>();
     [SerializeField] private Image imageToChange;
     [SerializeField] private Image imageToChange2;
     [SerializeField] public GameObject continue_btn;
@@ -23,37 +22,60 @@ public class Win_Lose_Script : MonoBehaviour
 
     public static Win_Lose_Script instance;
 
+    private bool hasWon = false;
+    private Rigidbody2D player;
+
     private void Awake()
     {
         instance = this;
     }
 
+    private void Start()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        }
+    }
+
     public void OnTriggerLose()
     {
-        Debug.Log("bruh");
-        win_lose_panel.SetActive(true);
-        imageToChange.sprite = images[0];
-        imageToChange2.sprite = images2[0];
-        retrty_btn.SetActive(true);
-        continue_btn.SetActive(false);
-        menu_btn.SetActive(true);
-        upgrade_btn.SetActive(true);
-        Rigidbody2D player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        player.linearVelocity = new Vector2(0, 0);
-        player.simulated = false;
-        Cursor.visible = true;
-        DisplayMoney();
+        if (!hasWon)
+        {
+            win_lose_panel.SetActive(true);
+
+            imageToChange.sprite = images[0];
+            imageToChange2.sprite = images2[0];
+
+            retrty_btn.SetActive(true);
+            continue_btn.SetActive(false);
+            menu_btn.SetActive(true);
+            upgrade_btn.SetActive(true);
+
+            player.linearVelocity = new Vector2(0, 0);
+            player.simulated = false;
+
+            Cursor.visible = true;
+            DisplayMoney();
+        }
     }
 
     public void OnTriggerWin()
     {
+        hasWon = true;
         win_lose_panel.SetActive(true);
+
         imageToChange.sprite = images[1];
         imageToChange2.sprite = images2[1];
+
         continue_btn.SetActive(true);
         retrty_btn.SetActive(false);
         menu_btn.SetActive(false);
         upgrade_btn.SetActive(false);
+
+        player.linearVelocity = new Vector2(0, 0);
+        player.simulated = false;
+
         Cursor.visible = true;
         DisplayMoney();
     }
